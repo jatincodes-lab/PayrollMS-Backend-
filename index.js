e556a36f11311185d469be1bca1233a15fe8766a
jatincodes-lab <jraj786182@gmail.com>
2025-07-08 10:30:08 +0530

    add cros origin

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: "https://payroll-ms-backend.vercel.app/",
+    origin: "https://payroll-ms-backend.vercel.app",
     credentials: true,
   })
 );
